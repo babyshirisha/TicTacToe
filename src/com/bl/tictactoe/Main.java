4e8afd2e4307f52a1733bd3e483f7acb6163f91e
babyshirisha <babyshirisha24@gmail.com>
2022-07-08 23:02:24 +0530

    create tictactoe game

---
+++ src/com/bl/tictactoe/Main.java
@@ -0,0 +1,11 @@
+package com.bl.tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to tic tac toe game");
+        TicTacToe game = new TicTacToe();
+        game.Board();
+        game.showBoard();
+        //game.selectOption();
+    }
+}
